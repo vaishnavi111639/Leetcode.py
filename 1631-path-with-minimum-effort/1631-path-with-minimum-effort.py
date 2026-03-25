aafbaad24a2a4284e83494a15e996cdbf2f072b2
vaishnavi111639 <vaishu65429@gmail.com>
2026-03-25 20:25:56 +0530

    Time: 206 ms (97.78%), Space: 21 MB (70.2%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -0,0 +1,34 @@
+import heapq
+from typing import List
+
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        n, m = len(heights), len(heights[0])
+        
+        dist = [[float('inf')] * m for _ in range(n)]
+        dist[0][0] = 0
+        
+        pq = [(0, 0, 0)]
+        dirs = [(0,1),(0,-1),(1,0),(-1,0)]
+        
+        while pq:
+            effort, r, c = heapq.heappop(pq)
+            
+            if effort > dist[r][c]:
+                continue
+            
+            if r == n - 1 and c == m - 1:
+                return effort
+            
+            for dr, dc in dirs:
+                nr, nc = r + dr, c + dc
+                
+                if 0 <= nr < n and 0 <= nc < m:
+                    newe = max(effort, abs(heights[r][c] - heights[nr][nc]))
+                    
+                    if newe < dist[nr][nc]:
+                        dist[nr][nc] = newe
+                        heapq.heappush(pq, (newe, nr, nc))
+        
+        return 0
+        
